57aa71402e775ab500d67446d09e362d0b575a50
Phillipp Oliveira <info.phillipp@gmail.com>
2017-05-16 23:28:46 -0300

    Add pages controller and change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
+
+
+  # get 'pages/index'
+
+  get '/home', to: 'pages#home'
+  get '/profile', to: 'pages#profile'
+  get '/explore', to: 'pages#explore'
 
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
